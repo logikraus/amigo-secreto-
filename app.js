33d91c4081a94abef8929bd52fd47945dd01031a
Caio Kruas <caiokraus1977@gmail.com>
2025-03-06 17:30:39 -0300

    adicionar amigo

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 let amigos = [];
+
+
+function adicionarAmigo() {
+    const input = document.getElementById("amigo");
+    const nome = input.value.trim();
+
+    if (nome === "") {
+        alert("Por favor, insira um nome válido.");
+        return;
+    }
+
+    amigos.push(nome);
+    atualizarLista();
+    input.value = "";
+}
